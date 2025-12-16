d589faf7a9a879fd5bb326985bf2c48addc169dd
thedebuglaw <thedebuglaw@gmail.com>
2025-12-17 00:01:23 +0300

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +26,41 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
+{
+    View::composer('*', function ($view) {
+
+        if (!Auth::check()) {
+            return;
+        }
+
+        $user = Auth::user();
+
+        $permissions = [
+
+            // Patients
+            'register_patient' => in_array($user->user_type, ['admin', 'doctor', 'receptionist']),
+            'search_patient'   => in_array($user->user_type, ['admin', 'doctor', 'receptionist']),
+
+            // Reports
+            'view_reports'     => in_array($user->user_type, ['admin', 'doctor']),
+            'clinic_report'    => in_array($user->user_type, ['admin', 'doctor', 'receptionist']),
+            'view_statistics'  => in_array($user->user_type, ['admin', 'doctor']),
+
+            // Medicine
+            'issue_medicine'   => in_array($user->user_type, ['admin', 'pharmacist']),
+
+            // Attendance
+            'reset_user'       => $user->user_type === 'admin',
+
+            // Users
+            'register_user'    => $user->user_type === 'admin',
+
+            // Notices
+            'manage_notices'   => $user->user_type === 'admin',
+        ];
+
+        $view->with('permissions', $permissions);
+    });
+}
+
 }
